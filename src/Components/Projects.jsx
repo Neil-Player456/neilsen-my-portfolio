132c362372630505f9eebe3a4db3a28cb60e91b3
Neilsen <nmrubix11@gmail.com>
2026-01-28 22:35:37 -0700

    fix profile on omnicient, fix spacing, go live on all projects so they can be clicked on

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,39 +2,39 @@ import React from "react";
 
 const projectList = [
     {
-      title: "Omniscient (All About Games)",
-      description: "Complete front-end and back-end app with React & Flask.",
+      title: "Omniscient - All About Games",
+      description: "Complete fullstack app built with React, Flask, & JWT.",
       image: "/images/featured1.png",
       live: "https://omniscient-live-site.com",
-      github: "https://github.com/yourname/omniscient",
+      github: "https://github.com/Neil-Player456/Neil--Omniscient.git",
     },
     {
-      title: "Hair Blesser (Booking Site)",
-      description: "Another end-to-end project with authentication and database.",
+      title: "Hair Blesser Booking Site",
+      description: "Complete fullstack app built with React, & Python, .",
       image: "/images/featured2.png",
       live: "https://hairblesser.com",
-      github: "https://github.com/yourname/hair-blesser",
+      github: "https://github.com/Neil-Player456/Hair-Blesser-Booking-Site.git",
     },
     {
-      title: "Viltrumite Mission List (Todo-List)",
-      description: "A smaller utility or fun project.",
+      title: "Viltrumite Mission List - Todo-List",
+      description: "Viltrumite Mission Log .",
       image: "/images/small1.png",
       live: "https://viltrumite-todo.netlify.app",
-      github: "https://github.com/yourname/viltrumite-todo",
+      github: "https://github.com/Neil-Player456/Neil-react-To-Do-list.git",
     },
     {
       title: "Random Card Generator",
-      description: "Another short project, focused on front-end.",
+      description: "Dynamic Deck Of Cards",
       image: "/images/small2.png",
       live: "https://random-card-gen.netlify.app",
-      github: "https://github.com/yourname/random-card-generator",
+      github: "https://github.com/Neil-Player456/Neil-Random-Card-Generator.git",
     },
     {
-      title: "Reactive Stop Light",
-      description: "A small creative experiment or widget.",
+      title: "Dynamic Reactive Stop Light",
+      description: "Stop Light In The Rain.",
       image: "/images/small3.png",
       live: "https://stop-light-demo.netlify.app",
-      github: "https://github.com/yourname/stop-light",
+      github: "https://github.com/Neil-Player456/Neil-react-stop-light.git",
     },
   ];
 
